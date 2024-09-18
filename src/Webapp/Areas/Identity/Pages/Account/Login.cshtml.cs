ab910654ae5ceed9e5b6f6b4e65ed9f2d19c4b03
PierreLeBlond <pierre.lespingal@gmail.com>
2024-09-18 16:18:29 +0200

    fix: Redirect to previous page on login

--- src/Webapp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Webapp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -2,18 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using Webapp.Models;
 
 namespace Webapp.Areas.Identity.Pages.Account
